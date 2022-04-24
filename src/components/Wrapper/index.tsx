5b174b654169f3649da3ede33709e514e776cea5
Daniel Coles <me@danielcoles.co.uk>
2022-04-24 21:17:19 +0100

    :sparkles:  add footer

--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import Footer from "../Footer";
 
 interface WrapperProps {
   children?: React.ReactNode;
@@ -9,8 +10,9 @@ const Wrapper: React.FC<WrapperProps> = ({ children }) => {
   return (
     <div className="p-2 h-screen text-white">
       <motion.div initial="initial" animate="animate" exit={{ opacity: 0 }}>
-        <main>{children}</main>
+        <main className="min-h-screen">{children}</main>
       </motion.div>
+      <Footer />
     </div>
   );
 };
